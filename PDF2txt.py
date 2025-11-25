40526a549c869e15857e6bbd006f216bd9d3f774
avoineu <21160@ecam.be>
2025-11-25 20:57:02 +0100

    Add PDF processing script and update .gitignore for Zamzar API integration

---
+++ PDF2txt.py
@@ -0,0 +1,20 @@
+from zamzar import ZamzarClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("ZAMZAR_API_KEY")
+if not api_key:
+    raise ValueError("Pas de clé API trouvé")
+
+zamzar = ZamzarClient(api_key)
+
+input_pdf = "resumetest.pdf"      # PDF d'entrée
+target_format = "txt"             # Format de sortie
+output_folder = "."               # Dossier de sortie (ici: le même dossier)
+output_filename = "resumetest.txt"
+
+job = zamzar.convert(input_pdf, target_format)
+job.store(output_folder).delete_all_files()
+
+print(f"Conversion terminée. Le fichier texte est dans {output_folder}/{output_filename}")
